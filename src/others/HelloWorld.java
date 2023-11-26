545c62b374ae6f476e6ad6dfaca84fd727b11970
Ravi <ravi8737@gmail.com>
2023-11-26 11:08:57 +0530

    Updating Hello World message

--- src/others/HelloWorld.java
+++ src/others/HelloWorld.java
@@ -7,6 +7,6 @@ public class HelloWorld {
     public static void main(String []args){
         //Main is a method
         //Main is an entry point to java program/project
-        System.out.println("Hello World!");
+        System.out.println("Hello World! pushing into Git");
     }
 }
